a9461dcaa589e4509ba066d7f8aecd0f3366be0f
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-01-12 22:40:17 +0300

    m

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -6,13 +6,11 @@ import axios from 'axios';
 export class TelegramController {
   constructor(private readonly userService: UserService) {}
 
-  // Step 2: Webhook handler to receive updates from Telegram
   @Post('webhook')
   async handleTelegramUpdate(@Body() update: any) {
     const message = update.message;
     const yourBotUsername = 'moll_moll_bot'; // Your bot username
 
-    // Handle the /start command
     if (message && message.text && message.text.startsWith('/start')) {
       const referralCode = this.extractReferralCode(message.text);
 
@@ -20,49 +18,51 @@ export class TelegramController {
       await this.sendMessage(
         message.chat.id,
         'Welcome to Mull Mull \n Discover new connections, chat, and find matches that fit your vibe!',
-        `https://t.me/${yourBotUsername}?start=${referralCode}`, // Mini app URL with referral code
-        'https://t.me/mollmoll_chat' // Channel URL
+        `https://t.me/${yourBotUsername}?start=${referralCode}`,
+        'https://t.me/mollmoll_chat'
       );
     }
   }
-        //'AgACAgQAAxkBAAECdbRng2ht0WiRYZE9iMKIQuEJG8O0_QACyMUxGydWGVCyLHmVUZz5yQEAAwIAA20AAzYE', // file_id of the image
 
-  // Step 3: Extract the referral code from the /start command
   private extractReferralCode(text: string): string {
-    const params = text.split(' ');
-    return params.length > 1 ? params[1] : null;
+    const match = text.match(/start=([a-zA-Z0-9\-]+)/);
+    return match ? match[1] : null;
   }
 
-  // Step 4: Function to send message with image, caption, and inline buttons
   private async sendMessage(chatId: string, text: string, miniAppUrl: string, channelUrl: string) {
     const token = '7629971501:AAGXQE13v9Anu6Gf8hRbVKYeCnHhppyA_Ko'; // Replace with your bot's token
-    const url = `https://api.telegram.org/bot${token}/sendPhoto`; // Sending a photo message
+    const url = `https://api.telegram.org/bot${token}/sendPhoto`;
 
-    const resualt =  await axios.post(url, {
-      chat_id: chatId,
-      //photo: fileId, // Using the file_id instead of a URL
-      caption: text, // Caption under the image
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: 'Open Mini App',
-              url: miniAppUrl, // Mini app deep link with referral code
-            },
-            {
-              text: 'Open Channel',
-              url: channelUrl, // Link to your channel
-            },
+    const photoUrl = 'https://copychic.online/uploads/profile-pictures/1736710779472-c0754815.webp'; // Example file_id
+
+    const result = await axios.post(
+      url,
+      {
+        chat_id: chatId,
+        photo: photoUrl, 
+        caption: text, 
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Open Mini App',
+                url: miniAppUrl,
+              },
+              {
+                text: 'Open Channel',
+                url: channelUrl,
+              },
+            ],
           ],
-        ],
+        },
       },
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(resualt)
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
+    console.log(result.data);
   }
-
 }
