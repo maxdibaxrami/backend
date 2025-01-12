97ef1a37ee90f569ec04aac90a0a204878341783
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-01-12 10:10:21 +0300

    m

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -9,11 +9,21 @@ export class TelegramController {
   @Post('webhook')
   async handleTelegramUpdate(@Body() update: any) {
     const message = update.message;
+    const yourBotUsername = 'moll_moll_bot';
 
-      let welcomeMessage = `Welcome, ! You have been successfully registered.`;
+    if (message && message.text && message.text.startsWith('/start')) {
+      const referralCode = this.extractReferralCode(message.text);
 
       // Send the message to the user
-      await this.sendMessage(message.chat.id, welcomeMessage);
+      await this.sendMessage(
+        message.chat.id,
+        'AgACAgQAAxkBAAECdbRng2ht0WiRYZE9iMKIQuEJG8O0_QACyMUxGydWGVCyLHmVUZz5yQEAAwIAA20AAzYE', // Image URL
+        'Welcome to Mull Mull \n Discover new connections, chat, and find matches that fit your vibe!',
+        `https://t.me/${yourBotUsername}?start=${referralCode}`, // Mini app URL with referral code
+        'https://t.me/mollmoll_chat' // Channel URL
+      );
+      
+    }
     
   }
 
@@ -22,17 +32,34 @@ export class TelegramController {
     return urlParams.get('start');
   }
 
-  private async sendMessage(chatId: string, text: string) {
+  private async sendMessage(chatId: string, fileId: string, text: string, miniAppUrl: string, channelUrl: string) {
     const token = '7629971501:AAGXQE13v9Anu6Gf8hRbVKYeCnHhppyA_Ko'; // Use your bot's token
-    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    const url = `https://api.telegram.org/bot${token}/sendPhoto`;
     
     await axios.post(url, {
       chat_id: chatId,
-      text: text,
+      photo: fileId, // Using the file_id instead of a URL
+      caption: text,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Open Mini App',
+              url: miniAppUrl,
+            },
+            {
+              text: 'Open Channel',
+              url: channelUrl,
+            },
+          ],
+        ],
+      },
     }, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    
   }
+  
 }
